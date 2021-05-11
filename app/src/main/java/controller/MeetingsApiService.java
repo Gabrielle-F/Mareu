bc76f6d65ee7e7a3a8c3103d5b4da35b92433ad7
Gabrielle Freville <gfreville5@gmail.com>
2021-05-11 22:24:36 +0200

    Fonctionnalité d'ajout complétée

--- app/src/main/java/controller/MeetingsApiService.java
+++ app/src/main/java/controller/MeetingsApiService.java
@@ -1,11 +1,16 @@
 package controller;
 
+import java.util.List;
+
 import gabrielle.freville.mareu1.Meetings;
+import gabrielle.freville.mareu1.MeetingsGenerator;
 
 public class MeetingsApiService implements ApiService {
 
+    private List<Meetings> mMeetings = MeetingsGenerator.DUMMY_MEETINGS;
+
     @Override
     public void createMeeting(Meetings meetings) {
-
+            mMeetings.add(meetings);
     }
 }

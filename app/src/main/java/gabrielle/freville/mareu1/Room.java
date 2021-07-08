2317ab0339cf6189be51d2d252edc73d0bec0deb
Gabrielle Freville <gfreville5@gmail.com>
2021-07-08 16:32:03 +0200

    Ajout enum "Room"

---
+++ app/src/main/java/gabrielle/freville/mareu1/Room.java
@@ -0,0 +1,40 @@
+package gabrielle.freville.mareu1;
+
+
+import androidx.annotation.NonNull;
+
+import java.io.Serializable;
+
+public enum Room implements Serializable {
+    MARIO(R.color.brightred, "Mario"),
+    TOAD(R.color.lightred, "Toad"),
+    BOWSER(R.color.lightorange, "Bowser"),
+    DAISY(R.color.lightyellow, "Daisy"),
+    LUIGI(R.color.brightgreen, "Luigi"),
+    YOSHI(R.color.lightgreen, "Yoshi"),
+    MELODY(R.color.lightblue, "Melody"),
+    WARIO(R.color.lightpurple, "Wario"),
+    PEACH(R.color.lightpink, "Peach"),
+    LUMA(R.color.luma, "Luma");
+    int color;
+    String label;
+
+    Room(int color, String label) {
+        this.color = color;
+        this.label = label;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return label;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}

e3728ef6b57e1ad57826c3ec3f05af74b59b57b1
Gabrielle Freville <gfreville5@gmail.com>
2021-10-20 14:30:24 +0200

    Add unit tests for add meetings, delete meetings and get meetings

---
+++ app/src/test/java/gabrielle/freville/mareu1/UnitTest.java
@@ -0,0 +1,45 @@
+package gabrielle.freville.mareu1;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import gabrielle.freville.mareu1.api.DependencyInjection;
+import gabrielle.freville.mareu1.api.MeetingApiService;
+import gabrielle.freville.mareu1.api.MeetingGenerator;
+import gabrielle.freville.mareu1.model.Meeting;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+
+    public MeetingApiService service;
+    public Meeting meeting;
+
+    @Before
+    public void setup(){
+        service = DependencyInjection.getMeetingsApiService();
+    }
+
+    @Test
+    public void addMeetingWithSuccess(){
+        Meeting meetingToAdd = meeting;
+        service.createMeeting(meetingToAdd);
+        assertTrue(service.getMeetings().contains(meetingToAdd));
+    }
+
+    @Test
+    public void deleteMeetingWithSuccess(){
+        Meeting meetingToDelete = service.getMeetings().get(1);
+        service.deleteMeeting(meetingToDelete);
+        assertFalse(service.getMeetings().contains(meetingToDelete));
+    }
+
+    @Test
+    public void getMeetingsWithSuccess(){
+        List<Meeting> meetings = service.getMeetings();
+        List<Meeting> expectedMeetings = MeetingGenerator.DUMMY_MEETINGS;
+        assertEquals(meetings, expectedMeetings);
+    }
+}

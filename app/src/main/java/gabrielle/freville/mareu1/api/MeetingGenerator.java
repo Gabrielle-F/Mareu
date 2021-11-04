f88680ef5e342e2793b958b373bc0366b59b0e64
Gabrielle Freville <gfreville5@gmail.com>
2021-11-04 21:52:40 +0100

    Add subject attribute to meetings

--- app/src/main/java/gabrielle/freville/mareu1/api/MeetingGenerator.java
+++ app/src/main/java/gabrielle/freville/mareu1/api/MeetingGenerator.java
@@ -10,15 +10,15 @@ import gabrielle.freville.mareu1.model.Meeting;
 public class MeetingGenerator {
 
     public static List<Meeting> DUMMY_MEETINGS = Arrays.asList(
-            new Meeting("25/06/2021", "14h00", Room.PEACH, "maxime@lamzone.com, alex@lamzone.com"),
-            new Meeting("25/06/2021", "16h00", Room.MARIO, "paul@lamzone.com, viviane@lamzone.com"),
-            new Meeting("25/06/2021", "19h00", Room.LUIGI, "viviane@lamzone.com, luc@lamzone.com"),
-            new Meeting("25/07/2021", "12h00", Room.WARIO, "luc@lamzone.com, maxime@lamzone.com"),
-            new Meeting("25/08/2021", "11hOO", Room.MELODY, "paul@lamzone.com, viviane@lamzone.com"),
-            new Meeting("25/07/2021", "09h00", Room.BOWSER, "alex@lamzone.com, maxime@lamzone.com"),
-            new Meeting("25/08/2021", "16h30", Room.YOSHI, "luc@lamzone.com, viviane@lamzone.com"),
-            new Meeting("25/06/2021", "14h20", Room.TOAD, "viviane@lamzone.com, paul@lamzone.com"),
-            new Meeting("25/07/2021", "11h40", Room.PEACH, "maxime@lamzone.com, alex@lamzone.com")
+            new Meeting("Réunion A", "25/06/2021", "14h00", Room.PEACH, "maxime@lamzone.com, alex@lamzone.com"),
+            new Meeting("Réunion B", "25/06/2021", "16h00", Room.MARIO, "paul@lamzone.com, viviane@lamzone.com"),
+            new Meeting("Réunion C", "25/06/2021", "19h00", Room.LUIGI, "viviane@lamzone.com, luc@lamzone.com"),
+            new Meeting("Réunion D", "25/07/2021", "12h00", Room.WARIO, "luc@lamzone.com, maxime@lamzone.com"),
+            new Meeting("Réunion E", "25/08/2021", "11hOO", Room.MELODY, "paul@lamzone.com, viviane@lamzone.com"),
+            new Meeting("Réunion F", "25/07/2021", "09h00", Room.BOWSER, "alex@lamzone.com, maxime@lamzone.com"),
+            new Meeting("Réunion G", "25/08/2021", "16h30", Room.YOSHI, "luc@lamzone.com, viviane@lamzone.com"),
+            new Meeting("Réunion H", "25/06/2021", "14h20", Room.TOAD, "viviane@lamzone.com, paul@lamzone.com"),
+            new Meeting("Réunion I", "25/07/2021", "11h40", Room.PEACH, "maxime@lamzone.com, alex@lamzone.com")
 
     );
 

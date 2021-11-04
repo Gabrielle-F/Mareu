b0e2cbb5daa5ab89ca97d1b6911b4f5fc268970f
Gabrielle Freville <gfreville5@gmail.com>
2021-11-04 02:52:09 +0100

    Correction names of participants

--- app/src/main/java/gabrielle/freville/mareu1/api/MeetingGenerator.java
+++ app/src/main/java/gabrielle/freville/mareu1/api/MeetingGenerator.java
@@ -12,13 +12,13 @@ public class MeetingGenerator {
     public static List<Meeting> DUMMY_MEETINGS = Arrays.asList(
             new Meeting("25/06/2021", "14h00", Room.PEACH, "maxime@lamzone.com, alex@lamzone.com"),
             new Meeting("25/06/2021", "16h00", Room.MARIO, "paul@lamzone.com, viviane@lamzone.com"),
-            new Meeting("25/06/2021", "19h00", Room.LUIGI, "amandine@lamzone.com, luc@lamzone.com"),
-            new Meeting("25/07/2021", "12h00", Room.WARIO, "gfrev5@lamzone.com, yan10@lamzone.com"),
-            new Meeting("25/08/2021", "11hOO", Room.MELODY, "una@lamzone.com, vivian@lamzone.com"),
-            new Meeting("25/07/2021", "09h00", Room.BOWSER, "alex@lamzone.com, chloe@lamzone.com"),
-            new Meeting("25/08/2021", "16h30", Room.YOSHI, "jay@lamzone.com, manon@lamzone.com"),
-            new Meeting("25/06/2021", "14h20", Room.TOAD, "yann@lamzone.com, vale@lamzone.com"),
-            new Meeting("25/07/2021", "11h40", Room.PEACH, "jay@lamzone.com, nico@lamzone.com")
+            new Meeting("25/06/2021", "19h00", Room.LUIGI, "viviane@lamzone.com, luc@lamzone.com"),
+            new Meeting("25/07/2021", "12h00", Room.WARIO, "luc@lamzone.com, maxime@lamzone.com"),
+            new Meeting("25/08/2021", "11hOO", Room.MELODY, "paul@lamzone.com, viviane@lamzone.com"),
+            new Meeting("25/07/2021", "09h00", Room.BOWSER, "alex@lamzone.com, maxime@lamzone.com"),
+            new Meeting("25/08/2021", "16h30", Room.YOSHI, "luc@lamzone.com, viviane@lamzone.com"),
+            new Meeting("25/06/2021", "14h20", Room.TOAD, "viviane@lamzone.com, paul@lamzone.com"),
+            new Meeting("25/07/2021", "11h40", Room.PEACH, "maxime@lamzone.com, alex@lamzone.com")
 
     );
 

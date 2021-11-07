d8752e29e9ee315e12e0215931aaad4acedecc91
Gabrielle Freville <gfreville5@gmail.com>
2021-11-07 19:20:49 +0100

    Add test for filter meetings

--- app/src/test/java/gabrielle/freville/mareu1/UnitTests.java
+++ app/src/test/java/gabrielle/freville/mareu1/UnitTests.java
@@ -3,23 +3,29 @@ package gabrielle.freville.mareu1;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import gabrielle.freville.mareu1.api.DependencyInjection;
 import gabrielle.freville.mareu1.api.MeetingApiService;
 import gabrielle.freville.mareu1.api.MeetingGenerator;
 import gabrielle.freville.mareu1.model.Meeting;
+import gabrielle.freville.mareu1.model.Room;
+import gabrielle.freville.mareu1.ui.MainActivity;
 
 import static org.junit.Assert.*;
 
 public class UnitTests {
 
+    public MainActivity mainActivity;
+    public Room room = Room.YOSHI ;
+    public String date = "10/10/2021";
     public MeetingApiService service;
     public Meeting meeting;
 
     @Before
     public void setup(){
-        service = DependencyInjection.getMeetingsApiService();
+        service = DependencyInjection.getMeetingApiService();
     }
 
     @Test
@@ -41,5 +47,16 @@ public class UnitTests {
         List<Meeting> meetings = service.getMeetings();
         List<Meeting> expectedMeetings = MeetingGenerator.DUMMY_MEETINGS;
         assertEquals(meetings, expectedMeetings);
+    }
+
+    @Test
+    public void filterMeetingsWithSuccess(){
+        ArrayList<Meeting> listToFilter = service.getMeetings();
+        ArrayList<Meeting> filteredMeetingList = new ArrayList<>();
+        mainActivity.filteringMeetings(listToFilter, room, date);
+        Meeting meetingToAdd = meeting;
+        assertSame(meetingToAdd.getRoom(), room);
+        assertEquals(meetingToAdd.getDate(), date);
+        assertTrue(filteredMeetingList.contains(meetingToAdd));
     }
 }

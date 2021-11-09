857a0875e6897807b4eb8ba7e7a5566bba62fe61
Gabrielle Freville <gfreville5@gmail.com>
2021-11-09 16:41:48 +0100

    Correction hour text

--- app/src/main/java/gabrielle/freville/mareu1/api/MeetingGenerator.java
+++ app/src/main/java/gabrielle/freville/mareu1/api/MeetingGenerator.java
@@ -14,7 +14,7 @@ public class MeetingGenerator {
             new Meeting("Réunion B", "25/06/2021", "16h00", Room.MARIO, "paul@lamzone.com, viviane@lamzone.com"),
             new Meeting("Réunion C", "25/06/2021", "19h00", Room.LUIGI, "viviane@lamzone.com, luc@lamzone.com"),
             new Meeting("Réunion D", "25/07/2021", "12h00", Room.WARIO, "luc@lamzone.com, maxime@lamzone.com"),
-            new Meeting("Réunion E", "25/08/2021", "11hOO", Room.MELODY, "paul@lamzone.com, viviane@lamzone.com"),
+            new Meeting("Réunion E", "25/08/2021", "11h00", Room.MELODY, "paul@lamzone.com, viviane@lamzone.com"),
             new Meeting("Réunion F", "25/07/2021", "09h00", Room.BOWSER, "alex@lamzone.com, maxime@lamzone.com"),
             new Meeting("Réunion G", "25/08/2021", "16h30", Room.YOSHI, "luc@lamzone.com, viviane@lamzone.com"),
             new Meeting("Réunion H", "25/06/2021", "14h20", Room.TOAD, "viviane@lamzone.com, paul@lamzone.com"),

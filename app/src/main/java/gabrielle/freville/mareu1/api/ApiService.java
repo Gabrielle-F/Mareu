af73214474f1bae90da1af4b8ccf640ebb482c7e
Gabrielle Freville <gfreville5@gmail.com>
2021-11-16 20:04:29 +0100

    Declaration and implementation of methods for filter meetings

--- app/src/main/java/gabrielle/freville/mareu1/api/ApiService.java
+++ app/src/main/java/gabrielle/freville/mareu1/api/ApiService.java
@@ -1,8 +1,10 @@
 package gabrielle.freville.mareu1.api;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import gabrielle.freville.mareu1.model.Meeting;
+import gabrielle.freville.mareu1.model.Room;
 
 public interface ApiService {
 
@@ -11,5 +13,14 @@ public interface ApiService {
     ArrayList<Meeting> getMeetings();
 
     void deleteMeeting(Meeting meeting);
+
+    //** Add after viva **/
+    ArrayList<Meeting> filteringMeetings(List<Meeting> listToFilter, Room selectedRoom, String selectedDate);
+
+    //** Add after viva **/
+    void confirmFilter(Room selectedRoom, String selectedDate);
+
+    //** Add after viva */
+    void clearFilter(Room room, String date);
 
 }
--- app/src/main/java/gabrielle/freville/mareu1/api/MeetingApiService.java
+++ app/src/main/java/gabrielle/freville/mareu1/api/MeetingApiService.java
@@ -4,13 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import gabrielle.freville.mareu1.model.Meeting;
+import gabrielle.freville.mareu1.model.Room;
+import gabrielle.freville.mareu1.ui.FilterMeetingsDialogFragment;
 
 public class MeetingApiService implements ApiService {
 
     private List<Meeting> meetings;
+    private FilterMeetingsDialogFragment dialogFragment;
 
     public MeetingApiService() {
         meetings = new ArrayList<>(MeetingGenerator.getMeetings());
+        dialogFragment = new FilterMeetingsDialogFragment();
     }
 
     @Override
@@ -26,6 +30,50 @@ public class MeetingApiService implements ApiService {
     @Override
     public void deleteMeeting(Meeting meeting) {
         meetings.remove(meeting);
+    }
+
+    //** Add after viva **/
+    @Override
+    public ArrayList<Meeting> filteringMeetings(List<Meeting> listToFilter, Room selectedRoom, String selectedDate) {
+        ArrayList<Meeting> meetingArrayList = new ArrayList<>();
+        for (Meeting meeting : listToFilter) {
+            if (selectedRoom != null && selectedDate != null) {
+                if (meeting.getRoom().toString().equals(selectedRoom.toString()) && meeting.getDate().equals(selectedDate)) {
+                    meetingArrayList.add(meeting);
+                }
+            }
+            if (selectedRoom != null && selectedDate == null && meeting.getRoom() == selectedRoom) {
+                meetingArrayList.add(meeting);
+            }
+            if (selectedRoom == null && selectedDate != null) {
+                if (meeting.getDate().equals(selectedDate)) {
+                    meetingArrayList.add(meeting);
+                }
+            }
+        }
+        return meetingArrayList;
+    }
+
+    //** Add after viva **/
+    @Override
+    public void confirmFilter(Room selectedRoom, String selectedDate) {
+        selectedRoom = dialogFragment.getRoomSpinner();
+        if (selectedRoom.toString().isEmpty()) {
+            selectedRoom = null;
+        }
+        selectedDate = dialogFragment.getSelectedDate();
+        if (selectedDate.isEmpty()) {
+            selectedDate = null;
+        }
+    }
+
+    //** Add after viva **/
+    @Override
+    public void clearFilter(Room room, String date) {
+        room = dialogFragment.getRoomSpinner();
+        date = dialogFragment.getSelectedDate();
+        room = null;
+        date = null;
     }
 
 }
